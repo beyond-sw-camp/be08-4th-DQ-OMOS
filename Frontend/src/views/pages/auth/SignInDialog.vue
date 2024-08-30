360ad146a10e847b8d8db2c49847919b57ed7fd4
Jongwon Kim <growjong8802@gmail.com>
2024-08-30 18:31:11 +0900

    design : topbar 버튼아이콘 수정 및 auth Dialog 파일 추가

---
+++ Frontend/src/views/pages/auth/SignInDialog.vue
@@ -0,0 +1,12 @@
+<template>
+    <Dialog header="Dialog" v-model:visible="display" :breakpoints="{ '960px': '75vw' }" :style="{ width: '30vw' }" :modal="true">
+        <p class="leading-normal m-0">로그인</p>
+        <template #footer>
+            <Button label="Save" @click="close" />
+        </template>
+    </Dialog>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
