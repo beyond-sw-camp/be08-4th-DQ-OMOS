97d3d8e764340de60e16420eff34fe7cf2196478
jongwon <growjong8802@gmail.com>
2024-09-01 04:29:08 +0900

    refactor : Backend 폴더 및 authService 추가

---
+++ Backend/authService/src/main/java/com/beyond/auth/entity/enums/Status.java
@@ -0,0 +1,6 @@
+package com.beyond.auth.entity.enums;
+
+public enum Status {
+    Y,  // 활성 회원
+    N   // 탈퇴 회원
+}
---
+++ Backend/authService/src/main/java/com/beyond/auth/service/CustomOAuth2UserService.java
@@ -0,0 +1,75 @@
+package com.beyond.auth.service;
+
+import com.beyond.auth.dto.oAuth2.*;
+import com.beyond.auth.entity.Member;
+import com.beyond.auth.entity.enums.Status;
+import com.beyond.auth.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String clientName = userRequest.getClientRegistration().getClientName();
+
+        OAuth2Response response = null;
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        if (clientName.equals("naver")) {
+            response = new NaverResponse(attributes);
+        } else if (clientName.equals("google")) {
+            response = new GoogleResponse(attributes);
+        } else {
+            return null;
+        }
+
+        String email = response.getEmail();
+        Optional<Member> isExist = memberRepository.findByEmail(email);
+
+        if (isExist.isPresent() && isExist.get().getStatus() == Status.N) {
+            memberRepository.deleteById(isExist.get().getMemberId());
+        }
+
+        saveOrUpdateUser(response, email);
+
+        OAuth2MemberDto oAuth2MemberDto = OAuth2MemberDto.builder()
+                .name(response.getName())
+                .email(email)
+                .role("ROLE_USER")
+                .build();
+
+        return new CustomOAuth2Member(oAuth2MemberDto);
+    }
+
+    private void saveOrUpdateUser(OAuth2Response response, String email) {
+        Optional<Member> isExist = memberRepository.findByEmail(email);
+
+        if (isExist.isPresent()) {
+            Member member = isExist.get();
+            member.setName(response.getName());
+            member.setEmail(response.getEmail());
+            memberRepository.save(member);
+        } else {
+            Member member = new Member();
+            member.setEmail(email);
+            member.setName(response.getName());
+            member.setPassword("OAuth2 인증 사용자");
+            memberRepository.save(member);
+        }
+    }
+}
+
