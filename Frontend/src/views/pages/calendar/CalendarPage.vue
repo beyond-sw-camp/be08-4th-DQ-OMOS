2c24fd93e763ef7ca71fbdecf83724f535d521f4
Jongwon Kim <growjong8802@gmail.com>
2024-09-02 19:14:31 +0900

    design : 캘린더 페이지 적용시 사이드바 간격 수정

--- Frontend/src/views/pages/calendar/CalendarPage.vue
+++ Frontend/src/views/pages/calendar/CalendarPage.vue
@@ -1,24 +1,35 @@
 <template>
     <div class="demo-app">
+        <!-- Sidebar -->
         <div class="demo-app-sidebar">
-            <div class="demo-app-sidebar-section">
-                <h2>사용법</h2>
-                <ul>
-                    <li>날짜를 클릭해서 이벤트를 추가할 수 있습니다.</li>
-                    <li>생성된 이벤트를 다른 날짜로 옮기거나, 당겨서 날짜를 늘릴 수 있습니다.</li>
-                    <li>클릭해서 이벤트를 삭제할 수 있습니다.</li>
-                </ul>
-            </div>
-            <div class="demo-app-sidebar-section">
-                <h2>All Events ({{ currentEvents.length }})</h2>
-                <ul>
-                    <li v-for="event in currentEvents" :key="event.id">
-                        <b>{{ event.startStr }}</b>
-                        <i>{{ event.title }}</i> - <span>{{ event.extendedProps.category }}</span>
-                    </li>
-                </ul>
-            </div>
+            <p-panel>
+                <template #header>
+                    사용법
+                </template>
+                <div>
+                    <ul>
+                        <li>날짜를 클릭해서 이벤트를 추가할 수 있습니다.</li>
+                        <li>생성된 이벤트를 다른 날짜로 옮기거나, 당겨서 날짜를 늘릴 수 있습니다.</li>
+                        <li>클릭해서 이벤트를 삭제할 수 있습니다.</li>
+                    </ul>
+                </div>
+            </p-panel>
+            <p-panel>
+                <template #header>
+                    All Events ({{ currentEvents.length }})
+                </template>
+                <div>
+                    <ul>
+                        <li v-for="event in currentEvents" :key="event.id">
+                            <b>{{ event.startStr }}</b>
+                            <i>{{ event.title }}</i> - <span>{{ event.extendedProps.category }}</span>
+                        </li>
+                    </ul>
+                </div>
+            </p-panel>
         </div>
+
+        <!-- Main Content -->
         <div class="demo-app-main">
             <FullCalendar class="demo-app-calendar" :options="calendarOptions">
                 <template v-slot:eventContent="arg">
@@ -27,8 +38,6 @@
                 </template>
             </FullCalendar>
         </div>
-        <EventModal :isOpen="isModalOpen" :initialData="selectedInfo ? { start: selectedInfo.startStr, end: selectedInfo.endStr } : {}" @save="saveEvent" @close="closeModal" />
-        <EventDetailModal :isOpen="isDetailModalOpen" :event="selectedEvent" @delete="deleteEvent" @close="closeDetailModal" />
     </div>
 </template>
 
@@ -42,12 +51,14 @@ import EventDetailModal from './EventDetailModal.vue';
 import EventModal from './EventModal.vue';
 import { INITIAL_EVENTS } from '/src/views/pages/calendar/utils/data.js';
 import { createEventId } from '/src/views/pages/calendar/utils/event-utils.js';
+import Panel from 'primevue/panel';
 
 export default defineComponent({
     components: {
         FullCalendar,
         EventModal,
-        EventDetailModal
+        EventDetailModal,
+        Panel
     },
     data() {
         return {
@@ -130,34 +141,11 @@ export default defineComponent({
 });
 </script>
 
-<style lang="css">
-h2 {
-    margin: 0;
-    font-size: 16px;
-}
-
-ul {
-    margin: 0;
-    padding: 0 0 0 1.5em;
-}
-
-li {
-    margin: 1.5em 0;
-    padding: 0;
-}
-
-b {
-    margin-right: 3px;
-}
-
+<style scoped>
 .demo-app {
     display: flex;
     min-height: 100%;
-    font-family:
-        Arial,
-        Helvetica Neue,
-        Helvetica,
-        sans-serif;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
     font-size: 14px;
 }
 
@@ -166,9 +154,6 @@ b {
     line-height: 1.5;
     background: #eaf9ff;
     border-right: 1px solid #d3e2e8;
-}
-
-.demo-app-sidebar-section {
     padding: 2em;
 }
 
