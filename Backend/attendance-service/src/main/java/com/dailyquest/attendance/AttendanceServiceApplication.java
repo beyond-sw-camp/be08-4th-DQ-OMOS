ef2e821ad1514eda79a287a9ea28d235252451f3
jongwon <growjong8802@gmail.com>
2024-09-02 22:58:42 +0900

    setting : attendance-service 개발 환경 세팅

---
+++ Backend/attendance-service/src/main/java/com/dailyquest/attendance/AttendanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dailyquest.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceServiceApplication.class, args);
+    }
+
+}
