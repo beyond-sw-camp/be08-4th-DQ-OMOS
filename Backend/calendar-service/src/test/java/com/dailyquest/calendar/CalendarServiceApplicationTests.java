321e662d5bd9077a32dd4bcaf18f6e7768ac0b39
tyeong1102 <tyeong1102@gmail.com>
2024-09-02 21:49:04 +0900

    setting : calendar-service 개발 환경 세팅

---
+++ Backend/calendar-service/src/test/java/com/dailyquest/calendar/CalendarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyquest.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
