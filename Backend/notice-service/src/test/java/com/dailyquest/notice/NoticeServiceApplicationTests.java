da2e27f0599de70c016fe42ba5c05c956a6e9b5d
tkckdnjs <wupby1@gmail.com>
2024-09-02 21:50:55 +0900

    setting : notice-service 개발 환경 세팅

---
+++ Backend/notice-service/src/test/java/com/dailyquest/notice/NoticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyquest.notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
