f78d913355940f1452b89b4bcd27c41929951710
jongwon <growjong8802@gmail.com>
2024-09-02 22:36:30 +0900

    refactor : auth-service 폴더구조 수정

--- Backend/authService/src/main/java/com/beyond/auth/entity/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.beyond.auth.entity.enums;
-
-public enum Role {
-    ROLE_ADMIN, // 관리자
-    ROLE_USER   // 회원
-}
-
--- Backend/authService/src/main/java/com/beyond/auth/entity/enums/Status.java
+++
@@ -1,6 +0,0 @@
-package com.beyond.auth.entity.enums;
-
-public enum Status {
-    Y,  // 활성 회원
-    N   // 탈퇴 회원
-}
