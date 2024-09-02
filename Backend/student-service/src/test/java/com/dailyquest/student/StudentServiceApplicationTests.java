5d78774efdfc7fead516da7ef77052d4de88cdee
mmvne <kingtjrals@naver.com>
2024-09-02 21:49:33 +0900

    setting: student-service 개발 환경 셋팅

---
+++ Backend/student-service/src/test/java/com/dailyquest/student/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyquest.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
