d9e00bf73b60a53632c86f55db87ec5d42674f13
jongwon <growjong8802@gmail.com>
2024-09-06 04:40:25 +0900

    setting : k8s 개발 환경 세팅 ingress 접근방식 추가 도커파일 수정 및 각 서비스 마다 webconfig 추가

--- Backend/calendar-service/src/main/java/com/dailyquest/calendar/config/WebConfig.java
+++ Backend/calendar-service/src/main/java/com/dailyquest/calendar/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.dailyquest.auth.config;
+package com.dailyquest.calendar.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
