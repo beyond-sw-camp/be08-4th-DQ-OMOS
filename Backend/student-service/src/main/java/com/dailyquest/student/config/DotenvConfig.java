6109edf46ffcefa5c2b382e3360976f1bb5ba766
mmvne <kingtjrals@naver.com>
2024-09-07 18:33:56 +0900

    tt13

--- Backend/student-service/src/main/java/com/dailyquest/student/config/DotenvConfig.java
+++ Backend/student-service/src/main/java/com/dailyquest/student/config/DotenvConfig.java
@@ -1,18 +1,17 @@
+package com.dailyquest.student.config;
+
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DotenvConfig {
 
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Dotenv dotenv = Dotenv.load();
-            System.setProperty("DB_URL", dotenv.get("DB_URL"));
-            System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-            System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        };
+    @PostConstruct
+    public void init() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
     }
 }
