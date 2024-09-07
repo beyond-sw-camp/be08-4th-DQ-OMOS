a7490482835e5e58a1ab71f94c1dd317dc2592fb
Jongwon Kim <growjong8802@gmail.com>
2024-09-07 19:13:05 +0900

    student-service 환경변수 수정

--- Backend/student-service/src/main/java/com/dailyquest/student/StudentServiceApplication.java
+++ Backend/student-service/src/main/java/com/dailyquest/student/StudentServiceApplication.java
@@ -9,19 +9,10 @@ public class StudentServiceApplication {
 
     public static void main(String[] args) {
 
-        // 로컬 환경에서만 .env 파일 로드
-        if (isLocalEnvironment()) {
-            Dotenv dotenv = Dotenv.load();
-            System.setProperty("DB_URL", dotenv.get("DB_URL"));
-            System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-            System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        }
+        System.setProperty("DB_URL", System.getenv("DB_URL"));
+        System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
 
         SpringApplication.run(StudentServiceApplication.class, args);
-    }
-
-    private static boolean isLocalEnvironment() {
-        // 로컬 환경을 판단하는 조건 (예: 특정 환경 변수가 설정되어 있지 않으면 로컬 환경으로 판단)
-        return System.getenv("CI") == null;  // GitHub Actions나 CI 시스템에서는 'CI' 환경 변수가 설정됨
     }
 }
