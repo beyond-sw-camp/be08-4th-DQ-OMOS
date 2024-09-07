e03d76b5db815929b1ee73c251054fce9e8985d7
mmvne <kingtjrals@naver.com>
2024-09-07 18:43:04 +0900

    tt15

--- Backend/student-service/src/test/java/com/dailyquest/student/StudentServiceApplicationTests.java
+++ Backend/student-service/src/test/java/com/dailyquest/student/StudentServiceApplicationTests.java
@@ -8,10 +8,6 @@ class StudentServiceApplicationTests {
 
     @Test
     void contextLoads() {
-        // 테스트 전에 환경 변수를 설정합니다.
-        System.setProperty("DB_URL", "jdbc:mariadb://omos-db.c9i2aecyi0tb.ap-northeast-2.rds.amazonaws.com:3306/testdb");
-        System.setProperty("DB_USERNAME", "beyond");
-        System.setProperty("DB_PASSWORD", "beyond");
     }
 
 }
