95aa1b49bad28a7caccc1ffb7443845f677b59f6
jongwon <growjong8802@gmail.com>
2024-09-10 01:49:12 +0900

    젠킨스 CICD 테스트 Backend swagger version 8.0.0 -> 10.0.0 Frontend title DailyQuest -> DailyQ

--- Backend/notice-service/src/main/java/com/dailyquest/notice/config/SwaggerConfig.java
+++ Backend/notice-service/src/main/java/com/dailyquest/notice/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "Notice Service",
                 description = "Notice Service API",
-                version = "v8.0.0"
+                version = "v10.0.0"
         )
 )
 public class SwaggerConfig {
