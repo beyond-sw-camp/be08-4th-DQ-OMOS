3fe54e067727cdffb0343ba20b8233c0b2b23b7d
Jongwon Kim <growjong8802@gmail.com>
2024-09-10 14:32:18 +0900

    fix : auth쪽 swagger 접속 문제 해결

--- Backend/auth-service/src/main/java/com/dailyquest/auth/AuthServiceApplication.java
+++ Backend/auth-service/src/main/java/com/dailyquest/auth/AuthServiceApplication.java
@@ -8,15 +8,15 @@ public class AuthServiceApplication {
 
     public static void main(String[] args) {
 
-        // 로컬 환경에서만 .env 파일을 로드할 필요 없이, 환경 변수를 그대로 사용
-        System.setProperty("DB_URL", System.getenv("DB_URL"));
-        System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
-        System.setProperty("NAVER_CLIENT_ID", System.getenv("NAVER_CLIENT_ID"));
-        System.setProperty("NAVER_CLIENT_SECRET", System.getenv("NAVER_CLIENT_SECRET"));
-        System.setProperty("GOOGLE_CLIENT_ID", System.getenv("GOOGLE_CLIENT_ID"));
-        System.setProperty("GOOGLE_CLIENT_SECRET", System.getenv("GOOGLE_CLIENT_SECRET"));
-        System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
+//        // 로컬 환경에서만 .env 파일을 로드할 필요 없이, 환경 변수를 그대로 사용
+//        System.setProperty("DB_URL", System.getenv("DB_URL"));
+//        System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+//        System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+//        System.setProperty("NAVER_CLIENT_ID", System.getenv("NAVER_CLIENT_ID"));
+//        System.setProperty("NAVER_CLIENT_SECRET", System.getenv("NAVER_CLIENT_SECRET"));
+//        System.setProperty("GOOGLE_CLIENT_ID", System.getenv("GOOGLE_CLIENT_ID"));
+//        System.setProperty("GOOGLE_CLIENT_SECRET", System.getenv("GOOGLE_CLIENT_SECRET"));
+//        System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
 
         SpringApplication.run(AuthServiceApplication.class, args);
     }
